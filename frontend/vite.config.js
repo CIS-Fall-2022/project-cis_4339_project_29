37e68f5d6e6f71fa23c988f9261900ed2c48edeb
Joseph Morris III <Joseph.morrisiii@hotmail.com>
2022-11-13 16:31:45 -0600

    Update vite.config.js
    Start frontend on PORT 8000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,22 @@
+import { fileURLToPath, URL } from 'url'
+
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8000,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
+/*import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const path = require("path");
 
@@ -10,4 +28,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}) */
