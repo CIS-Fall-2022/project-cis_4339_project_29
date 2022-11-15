90ddef6c6519fd6b54add714fefbb9d985e64cc4
Zachary Blackwell <zblackwe@CougarNet.UH.EDU>
2022-11-14 20:22:46 -0600

    Fixed Description being displayed in edit event.

--- frontend/src/components/eventDetails.vue
+++ frontend/src/components/eventDetails.vue
@@ -56,6 +56,8 @@
               <textarea
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 rows="2"
+                v-model="event.description"
+                type="text"
               ></textarea>
             </label>
           </div>
