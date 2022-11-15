c2542e8e4cabfb10f8dda85629fbf9846811c213
Joseph Morris III <Joseph.morrisiii@hotmail.com>
2022-11-14 22:57:20 -0600

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -37,6 +37,12 @@
                 Find Event
               </router-link>
             </li>
+            <li>
+              <router-link to="/chart">
+                <span style="position: relative; top: 6px" class="material-icons">dashboard</span>
+                Chart
+              </router-link>
+            </li>
           </ul>
         </nav>
       </header>
