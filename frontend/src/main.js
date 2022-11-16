d3693dd3635d3baf416f44d53637eb250cf04d91
Joseph Morris III <Joseph.morrisiii@hotmail.com>
2022-11-15 19:27:41 -0600

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 const app = createApp(App);
 app.use(router);
